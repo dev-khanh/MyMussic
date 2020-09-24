7cabc0d6c9596eac8167bd853a697122f79009da
Duong Quoc Khanh <devdqkhanh@gmail.com>
2020-09-24 17:33:44 +0700

    slider

--- src/compoment/Slider.js
+++ src/compoment/Slider.js
@@ -1,20 +1,20 @@
 /* eslint-disable prettier/prettier */
-import React, { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
 import {
-	Dimensions,
-	PanResponder,
-	View,
-	Animated,
-	Text,
-	Platform,
+  Dimensions,
+  PanResponder,
+  View,
+  Animated,
+  Text,
+  Platform,
 } from 'react-native';
-// import TrackPlayer from 'react-native-track-player';
-import Svg, { Circle, G, Path } from 'react-native-svg';
-// import {timeFormat, polarToCartesian, cartesianToPolar} from 'utils/index';
-import { Colors } from '../constants';
+import TrackPlayer from 'react-native-track-player';
+import Svg, {Circle, G, Path} from 'react-native-svg';
+import {timeFormat, polarToCartesian, cartesianToPolar} from '../utils/';
+import {Colors} from '../constants';
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 const padding = 20;
 const r = (width - padding * 2) / 2;
 const cx = r + padding;
@@ -25,184 +25,183 @@ let interval;
 
 const STATE_READY = Platform.OS === 'ios' ? 'ready' : 6;
 
-export default function Slider({ positionY, miniPos }) {
-	//   const {state, track, playing} = useSelector((state) => state.Player);
-	const [percent, setPercent] = useState(0);
-	const [time, setTime] = useState(null);
-	const [moveSlider, setMoveSlider] = useState(false);
-	const [duration, setDuration] = useState(null);
+export default function Slider({positionY, miniPos}) {
+  // const {state, track, playing} = useSelector((state) => state.Player);
+  //   const {track} = useSelector((state) => state.Player);
+  const [percent, setPercent] = useState(0);
+  const [time, setTime] = useState(null);
+  const [moveSlider, setMoveSlider] = useState(false);
+  const [duration, setDuration] = useState(null);
+  //   const [changePlaybackTrack] = useState(null);
+  useEffect(() => {
+        // clearInterval(interval);
 
-	//   useEffect(() => {
-	//     clearInterval(interval);
+    //     const times = async () => {
+    //     //   const current = Math.floor(await TrackPlayer.getPosition());
 
-	//     const times = async () => {
-	//     //   const current = Math.floor(await TrackPlayer.getPosition());
+    //       if (!playing || current < 1) {
+    //         setPercent(0);
+    //         setTime(0);
+    //       }
+    //     };
 
-	//       if (!playing || current < 1) {
-	//         setPercent(0);
-	//         setTime(0);
-	//       }
-	//     };
+    //     switch (state) {
+    //       case STATE_READY:
+    //         // user playing state
 
-	//     switch (state) {
-	//       case STATE_READY:
-	//         // user playing state
+    //         times();
+    //         break;
+    //       case TrackPlayer.STATE_PLAYING:
+    //         if (!moveSlider) {
+    //           interval = setInterval(async () => {
+    //             const current = Math.floor(await TrackPlayer.getPosition());
 
-	//         times();
-	//         break;
-	//       case TrackPlayer.STATE_PLAYING:
-	//         if (!moveSlider) {
-	//           interval = setInterval(async () => {
-	//             const current = Math.floor(await TrackPlayer.getPosition());
+    //             if (duration && current) {
+    //               setTime(current);
+    //               setPercent((current * 100) / Math.floor(duration));
+    //             }
+    //           }, 100);
+    //         }
+    //         break;
+    //     }
+    changePlaybackTrack();
+  }, [moveSlider]);
 
-	//             if (duration && current) {
-	//               setTime(current);
-	//               setPercent((current * 100) / Math.floor(duration));
-	//             }
-	//           }, 100);
-	//         }
-	//         break;
-	//     }
-	//   }, [state, moveSlider]);
+  //   useEffect(() => {
+  //     changePlaybackTrack();
+  //   }, [track]);
+  // changePlaybackTrack();
+  const changePlaybackTrack = async () => {
+    const currentDuration = await TrackPlayer.getDuration();
+    setDuration(currentDuration);
+  };
 
-	// useEffect(() => {
-	// 	changePlaybackTrack();
-	// }, [track]);
+  const setProgress = (x, y) => {
+    // if (!track || state === STATE_READY) {
+    //   return;
+    // }
 
-	const changePlaybackTrack = async () => {
-		// const currentDuration = await TrackPlayer.getDuration();
-		// setDuration(currentDuration);
-	};
+    const angleToPercent = (cartesianToPolar(x, y, {cy, cx}) / 180) * 100;
 
-	const setProgress = (x, y) => {
-		// if (!track || state === STATE_READY) {
-		//   return;
-		// }
+    setTime((duration / 100) * angleToPercent);
+    setPercent(angleToPercent);
 
-		// const angleToPercent = (cartesianToPolar(x, y, {cy, cx}) / 180) * 100;
+    return true;
+  };
 
-		// setTime((duration / 100) * angleToPercent);
-		// setPercent(angleToPercent);
+  const _panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => true,
+    onStartShouldSetPanResponderCapture: () => {
+      // start
+      setMoveSlider(true);
+    },
+    onPanResponderRelease: () => {
+      // end
+      TrackPlayer.seekTo((duration / 100) * percent);
 
-		return true;
-	};
+      setMoveSlider(false);
+    },
+    onPanResponderGrant: ({nativeEvent: {locationX, locationY}}) =>
+      setProgress(locationX, locationY),
+    onPanResponderMove: ({nativeEvent: {locationX, locationY}}) =>
+      setProgress(locationX, locationY),
+  });
 
-	const _panResponder = PanResponder.create({
-		onStartShouldSetPanResponder: () => true,
-		onMoveShouldSetPanResponder: () => true,
-		onStartShouldSetPanResponderCapture: () => {
-			// start
-			setMoveSlider(true);
-		},
-		onPanResponderRelease: () => {
-			// end
-			//   TrackPlayer.seekTo((duration / 100) * percent);
+  const {x, y} = polarToCartesian(
+    ((percent > 100 ? 100 : percent) * 180) / 100,
+    {cy, cx, r},
+  );
 
-			setMoveSlider(false);
-		},
-		onPanResponderGrant: ({ nativeEvent: { locationX, locationY } }) =>
-			setProgress(locationX, locationY),
-		onPanResponderMove: ({ nativeEvent: { locationX, locationY } }) =>
-			setProgress(locationX, locationY),
-	});
+  return (
+    <Animated.View style={styles.animatedView(positionY, miniPos)}>
+      <Text numberOfLines={1} style={moveSlider && styles.animatedText()}>
+        {timeFormat(time)}
+      </Text>
 
-	//   const {x, y} = polarToCartesian(
-	//     ((percent > 100 ? 100 : percent) * 180) / 100,
-	//     {cy, cx, r},
-	//   );
+      <Text numberOfLines={1} style={styles.duration}>
+        {timeFormat(duration === null ? null : Math.floor(duration))}
+      </Text>
 
-	return (
-		<Animated.View
-			style={styles.animatedView(positionY, miniPos)}>
-			<Text
-				numberOfLines={1}
-				style={moveSlider && styles.animatedText()}>
-				{/* {timeFormat(time)} */}
-			</Text>
+      <View>
+        <Svg width={'100%'} height={'100%'}>
+          <G {..._panResponder.panHandlers}>
+            <Path
+              fill="none"
+              stroke={Colors.gray}
+              strokeWidth={5}
+              d={`M${padding} ${cy} A ${r} ${r} 0 0 0 ${r * 2 + padding} ${cy}`}
+            />
 
-			<Text numberOfLines={1} style={styles.duration}>
-				{/* {timeFormat(duration === null ? null : Math.floor(duration))} */}
-			</Text>
+            <Path
+              fill="none"
+              strokeWidth={5}
+              stroke={Colors.primary}
+              d={`M${padding} ${cy} A ${r} ${r} 0 0 0 ${x} ${y}`}
+            />
 
-			<View>
-				<Svg width={'100%'} height={'100%'}>
-					<G {..._panResponder.panHandlers}>
-						<Path
-							fill="none"
-							stroke={Colors.gray}
-							strokeWidth={5}
-							d={`M${padding} ${cy} A ${r} ${r} 0 0 0 ${r * 2 + padding} ${cy}`}
-						/>
-
-						{/* <Path
-							fill="none"
-							strokeWidth={5}
-							stroke={Colors.primary}
-							d={`M${padding} ${cy} A ${r} ${r} 0 0 0 ${x} ${y}`}
-						/> */}
-
-						{/* <Circle
+            <Circle
               cx="0"
               cy="0"
               r={10}
-            //   x={Math.abs(x)}
+              x={Math.abs(x)}
               y={Math.abs(y)}
               fill={Colors.primary}
-            /> */}
-					</G>
+            />
+          </G>
 
-					<Circle
-						r={r - 30}
-						fill="none"
-						strokeWidth="2"
-						cx={r + padding}
-						cy={padding}
-					/>
-				</Svg>
-			</View>
-		</Animated.View>
-	);
+          <Circle
+            r={r - 30}
+            fill="none"
+            strokeWidth="2"
+            cx={r + padding}
+            cy={padding}
+          />
+        </Svg>
+      </View>
+    </Animated.View>
+  );
 }
 
 const styles = {
-	container: {
-		position: 'relative',
-		top: 220,
-		height,
-		zIndex: 3,
-	},
+  container: {
+    position: 'relative',
+    top: 220,
+    height,
+    zIndex: 3,
+  },
 
-	current: {
-		top: -10,
-		left: 0,
-		width: padding * 2 + 20,
-		position: 'absolute',
-		color: '#ddd',
-		textAlign: 'left',
-		fontSize: 12,
-		paddingLeft: 5,
-	},
+  current: {
+    top: -10,
+    left: 0,
+    width: padding * 2 + 20,
+    position: 'absolute',
+    color: '#ddd',
+    textAlign: 'left',
+    fontSize: 12,
+    paddingLeft: 5,
+  },
 
-	duration: {
-		top: -10,
-		right: 0,
-		width: padding * 2 + 20,
-		position: 'absolute',
-		color: '#ddd',
-		textAlign: 'right',
-		fontSize: 12,
-		paddingRight: 5,
-	},
-	animatedView: (positionY, miniPos) => ({
-		opacity: positionY.interpolate({
-			inputRange: [0, miniPos / 2, miniPos],
-			outputRange: [1, 0, 0],
-		}),
-		...styles.container,
-	}),
-	animatedText: () => ({
-		fontSize: 14,
-		color: Colors.primary,
-		...styles.current,
-	}),
+  duration: {
+    top: -10,
+    right: 0,
+    width: padding * 2 + 20,
+    position: 'absolute',
+    color: '#ddd',
+    textAlign: 'right',
+    fontSize: 12,
+    paddingRight: 5,
+  },
+  animatedView: (positionY, miniPos) => ({
+    opacity: positionY.interpolate({
+      inputRange: [0, miniPos / 2, miniPos],
+      outputRange: [1, 0, 0],
+    }),
+    ...styles.container,
+  }),
+  animatedText: () => ({
+    fontSize: 14,
+    color: Colors.primary,
+    ...styles.current,
+  }),
 };
